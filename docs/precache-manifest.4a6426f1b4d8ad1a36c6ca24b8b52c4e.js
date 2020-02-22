1ac5bfb2cef88c03ce9bf5f4c5159d2758a970d7
Paul Wrubel <pwrubel7@gmail.com>
2020-02-22 00:13:56 -0600

    missed some files

---
+++ docs/precache-manifest.4a6426f1b4d8ad1a36c6ca24b8b52c4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702999ec7f21f32658524f839406eb4f",
+    "url": "/vixuals/index.html"
+  },
+  {
+    "revision": "d6bc450beadea10e2e72",
+    "url": "/vixuals/static/css/main.4c04d491.chunk.css"
+  },
+  {
+    "revision": "eda860eddcc3edba1078",
+    "url": "/vixuals/static/js/2.d5fc0fdb.chunk.js"
+  },
+  {
+    "revision": "24d78eeaccabcfaf53f8f7030540fc58",
+    "url": "/vixuals/static/js/2.d5fc0fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6bc450beadea10e2e72",
+    "url": "/vixuals/static/js/main.57451059.chunk.js"
+  },
+  {
+    "revision": "2965da42477d0f5105f1",
+    "url": "/vixuals/static/js/runtime-main.563fc985.js"
+  }
+]);
