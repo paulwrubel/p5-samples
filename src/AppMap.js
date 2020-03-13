95b90b22d4dd16b8e0bd61fa0247423d49affc0b
Paul Wrubel <pwrubel7@gmail.com>
2020-03-13 00:22:44 -0500

    version updates

--- src/AppMap.js
+++ src/AppMap.js
@@ -45,7 +45,7 @@ const appMap = new Map([
         name: "triangles",
         displayName: "Triangles",
         component: TrianglesApp,
-        version: "0.1a",
+        version: "0.2a",
         description: "Triangles (2018)"
     }]
 ]);
