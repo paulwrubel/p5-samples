8587da44565a67c7782e526f09f0cd2d508ce5f7
Paul Wrubel <pwrubel7@gmail.com>
2020-03-19 10:49:55 -0500

    changed router

--- src/VixualsRouter.js
+++ src/VixualsRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { HashRouter, Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
 import Vixuals from 'Vixuals'
 
 const DEFAULT_PARAM = "__DEFAULT_APP__"
@@ -9,7 +9,7 @@ class VixualsRouter extends React.Component {
     render() {
         return (
             <div className="VixualsRouter">
-                <HashRouter>
+                <BrowserRouter>
                     <Switch>
                         <Route path="/:app">
                             <Vixuals
@@ -19,7 +19,7 @@ class VixualsRouter extends React.Component {
                             <Redirect to={`/${DEFAULT_PARAM}`}/>
                         </Route>
                     </Switch>
-                </HashRouter>
+                </BrowserRouter>
             </div>
         );
     }
